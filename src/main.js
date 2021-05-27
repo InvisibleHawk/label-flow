bee9c6b2b25caf715b18053fc8f266c19167f10a
Roman Minkin <minkinrd@mail.ru>
2021-05-27 22:46:40 +1000

    second test commit

--- src/main.js
+++ src/main.js
@@ -1,9 +1,40 @@
-const { app, BrowserWindow } = require('electron')
+const path = require('path')
+const { app, Menu, Tray, BrowserWindow } = require('electron')
 
 let mainWindow = null
+let tray = null
 
 app.on('ready', () => {
-  mainWindow = new BrowserWindow()
-  mainWindow.webContents.openDevTools()
+  mainWindow = new BrowserWindow({
+    height: 500,
+    width: 300,
+    frame: false,
+    resizable: false,
+    show: false,
+  })
+
   mainWindow.webContents.loadFile('src/mainWindow.html')
+
+  const iconName = 'ts-logo.png'
+  const iconPath = path.join(__dirname, `./assets/${iconName}`)
+  console.log(iconPath)
+  tray = new Tray(iconPath)
+  tray.setToolTip('My app')
+  tray.on('click', (event, bounds) => {
+    const { x, y } = bounds
+    const { height, width } = mainWindow.getBounds()
+
+    console.log(bounds.x, bounds.y)
+    if (mainWindow.isVisible()) {
+      mainWindow.hide()
+    } else {
+      mainWindow.setBounds({
+        x: Math.floor(x - width / 2),
+        y: Math.floor(y - height),
+        height,
+        width,
+      })
+      mainWindow.show()
+    }
+  })
 })
