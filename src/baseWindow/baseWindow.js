111aa0835d7a59654a57a31c3fecc748812fd862
Roman Minkin <minkinrd@mail.ru>
2021-06-12 21:05:54 +1000

    hamburger menu

--- src/baseWindow/baseWindow.js
+++ src/baseWindow/baseWindow.js
@@ -3,17 +3,25 @@ const closeBtn = document.querySelector('.btn-close')
 const menuBtn = document.querySelector('.call-menu')
 const menu = document.querySelector('.menu')
 
+const menu_items = {
+  main: 'Главное',
+  dbm: 'Клиенты',
+  finance: 'Финансы',
+  doc: 'Документы',
+}
+
 const blogMenu = `<div class="main-menu">
-<a href="#" class="menu-btn">Menu</a>
-<a href="#" class="menu-btn">Data Base</a>
-<a href="#" class="menu-btn">Statics</a>
-<a href="#" class="menu-btn">Documents</a>
+<a href="#" class="menu-btn">${menu_items.main}</a>
+<a href="#" class="menu-btn">${menu_items.dbm}</a>
+<a href="#" class="menu-btn">${menu_items.finance}</a>
+<a href="#" class="menu-btn">${menu_items.doc}</a>
 </div>`
 
 closeBtn.addEventListener('click', () => {
   remote.getCurrentWindow().hide()
 })
 
-menuBtn.addEventListener('click', () => {
-  menu.innerHTML = blogMenu
+menuBtn.addEventListener('click', (event) => {
+  if (menu.children.length === 0) menu.innerHTML = blogMenu
+  else menu.innerHTML = ''
 })
